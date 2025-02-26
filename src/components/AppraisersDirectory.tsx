3953c6e385d87e08f6e3caf6488583cebfd0f7c3
Appraisily <134934621+Appraisily@users.noreply.github.com>
2025-02-26 12:18:34 +0100

    aa

--- src/components/AppraisersDirectory.tsx
+++ src/components/AppraisersDirectory.tsx
@@ -3,27 +3,97 @@ import { MapPin, Star, Clock, Award, Badge } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 // Import all location data
+import aspenData from '../data/locations/aspen.json';
+import atlantaData from '../data/locations/atlanta.json';
+import austinData from '../data/locations/austin.json';
 import bostonData from '../data/locations/boston.json';
+import buffaloData from '../data/locations/buffalo.json';
+import charlestonData from '../data/locations/charleston.json';
+import charlotteData from '../data/locations/charlotte.json';
+import chicagoData from '../data/locations/chicago.json';
+import cincinnatiData from '../data/locations/cincinnati.json';
+import clevelandData from '../data/locations/cleveland.json';
+import columbusData from '../data/locations/columbus.json';
+import dallasData from '../data/locations/dallas.json';
+import denverData from '../data/locations/denver.json';
+import fortWorthData from '../data/locations/fort-worth.json';
+import hartfordData from '../data/locations/hartford.json';
 import houstonData from '../data/locations/houston.json';
+import indianapolisData from '../data/locations/indianapolis.json';
+import jacksonvilleData from '../data/locations/jacksonville.json';
+import kansasCityData from '../data/locations/kansas-city.json';
 import lasVegasData from '../data/locations/las-vegas.json';
 import miamiData from '../data/locations/miami.json';
+import minneapolisData from '../data/locations/minneapolis.json';
+import nashvilleData from '../data/locations/nashville.json';
+import newOrleansData from '../data/locations/new-orleans.json';
+import newYorkData from '../data/locations/new-york.json';
+import palmBeachData from '../data/locations/palm-beach.json';
+import philadelphiaData from '../data/locations/philadelphia.json';
+import phoenixData from '../data/locations/phoenix.json';
+import pittsburghData from '../data/locations/pittsburgh.json';
+import portlandData from '../data/locations/portland.json';
+import providenceData from '../data/locations/providence.json';
+import raleighData from '../data/locations/raleigh.json';
+import richmondData from '../data/locations/richmond.json';
+import sacramentoData from '../data/locations/sacramento.json';
+import saltLakeCityData from '../data/locations/salt-lake-city.json';
+import sanAntonioData from '../data/locations/san-antonio.json';
+import sanDiegoData from '../data/locations/san-diego.json';
 import sanFranciscoData from '../data/locations/san-francisco.json';
 import sanJoseData from '../data/locations/san-jose.json';
 import santaFeData from '../data/locations/santa-fe.json';
+import savannahData from '../data/locations/savannah.json';
 import seattleData from '../data/locations/seattle.json';
 import stLouisData from '../data/locations/st-louis.json';
+import washingtonData from '../data/locations/washington.json';
 
 // Combine all appraiser data
 const allLocations = {
+  aspen: aspenData,
+  atlanta: atlantaData,
+  austin: austinData,
   boston: bostonData,
+  buffalo: buffaloData,
+  charleston: charlestonData,
+  charlotte: charlotteData,
+  chicago: chicagoData,
+  cincinnati: cincinnatiData,
+  cleveland: clevelandData,
+  columbus: columbusData,
+  dallas: dallasData,
+  denver: denverData,
+  'fort-worth': fortWorthData,
+  hartford: hartfordData,
   houston: houstonData,
+  indianapolis: indianapolisData,
+  jacksonville: jacksonvilleData,
+  'kansas-city': kansasCityData,
   'las-vegas': lasVegasData,
   miami: miamiData,
+  minneapolis: minneapolisData,
+  nashville: nashvilleData,
+  'new-orleans': newOrleansData,
+  'new-york': newYorkData,
+  'palm-beach': palmBeachData,
+  philadelphia: philadelphiaData,
+  phoenix: phoenixData,
+  pittsburgh: pittsburghData,
+  portland: portlandData,
+  providence: providenceData,
+  raleigh: raleighData,
+  richmond: richmondData,
+  sacramento: sacramentoData,
+  'salt-lake-city': saltLakeCityData,
+  'san-antonio': sanAntonioData,
+  'san-diego': sanDiegoData,
   'san-francisco': sanFranciscoData,
   'san-jose': sanJoseData,
   'santa-fe': santaFeData,
+  savannah: savannahData,
   seattle: seattleData,
   'st-louis': stLouisData,
+  washington: washingtonData,
 };
 
 // Type definitions
